9c2a6354c913b4fbf27f66978dbffafb4afe98b5
Emma <diego.imprenti@issgreppi.it>
2023-11-29 16:53:51 +0100

    v0.2.2 Aggiunti tutti gli Hiragana, evita ripetizioni nel test, contatore di errori

--- Pages/hiraganaTest.js
+++ Pages/hiraganaTest.js
@@ -1,16 +1,40 @@
-var kana = ['a', 'i', 'u', 'e', 'o'];
+//var kana = ['a', 'i', 'u', 'e', 'o'];
+
+var kana = ['a', 'i', 'u', 'e', 'o', 'ka', 'ki', 'ku', 'ke', 'ko', 'sa', 'shi', 'su', 'se', 'so', 'ta', 'chi', 'tsu', 'te', 'to', 'na', 'ni', 'nu', 'ne', 'no', 'ha', 'hi', 'fu', 'he', 'ho', 'ma', 'mi', 'mu', 'me', 'mo', 'ya', 'yu', 'yo', 'ra', 'ri', 'ru', 're', 'ro', 'wa', 'wo', 'ga', 'gi', 'gu', 'ge', 'go', 'za', 'ji', 'zu', 'ze', 'zo', 'da', 'de', 'do', 'ba', 'bi', 'bu', 'be', 'bo', 'pa', 'pi', 'pu', 'pe', 'po',];
+var kanaLetti = []; //kana che sono gia stati "utilizzati"
+var kanaCorretti = []; //array dei kana che erano corretti
+var kanaSbagliati = []; //array dei kana che sono stati sbagliati
+
 
 function kanaCasuale() 
 {
-    return Math.floor(Math.random() * kana.length);; //creo un random che sceglie un indice dell'array kana 
+    return Math.floor(Math.random() * kana.length);; //creo un random che sceglie un indice dell'array kana
 }
 
 function primoKana() 
 {
     var immagine = document.getElementById("HiraganaPicture"); //creo l'elemento immagine
-    var risultato = `../Images/Hiragana/${kana[kanaCasuale()]}.png`; //utilizzo il metodo kanaCasuale per avere un kana random
+    var kanaRisultato = kana[kanaCasuale()]; //utilizzo il metodo kanaCasuale per avere un kana random
+    var risultato = `../Images/Hiragana/${kanaRisultato}.png`; //ho l'indirizzo dell'immagine del kana random che ho generato prima
     console.log(risultato); //debug
-    immagine.src = risultato; //applico il risultato ad immagine, cambiandola
+    
+    if(!kanaLetti.includes(kanaRisultato)) //se kanaLetti non contiene il kana generato randomicamente
+    {
+        kanaLetti.push(kanaRisultato); //aggiungo il kana all'array di quelli letti
+        immagine.src = risultato; //applico il risultato ad immagine, cambiandola
+    }
+    else //altrimenti
+    {
+        if(kana.length !== kanaLetti.length) //se la lunghezza di kanaLetti non e uguale a quella di kana
+        {
+            primoKana(); //riutilizzo la funzione (in maniera ricorsiva)
+        }
+        else //altrimenti
+        {
+            immagine.src = '../Images/test2.png'; //imposta l'immagine a test2 (debug)
+        }
+    }
+    
 }
 
 function tagliaStringa(testo)
@@ -36,41 +60,82 @@ function controllaTesto() {
     canvas.width = immagineNormale.width; //ottengo le dimensioni dell'immagine originale (altezza e larghezza)
     canvas.height = immagineNormale.height;
 
-    var context = canvas.getContext("2d"); //ottengo un rendering 2D dell'immagine (?)
+    var context = canvas.getContext("2d"); //ottengo un rendering 2D dell'immagine
 
     context.drawImage(immagineNormale, 0, 0); //disegno l'immagine sul canva
 
     if (testoInserito.toLowerCase() === kanaImmagine.toLowerCase()) { //controllo che il testo iserito (dopo averlo trasformato in lowercase per comodita) sia uguale al kana rappresentato (sempre in lowercase per sicurezza)
         console.log("Il testo è corretto!"); //debug
 
-        context.globalCompositeOperation = "source-in"; //le modifice al canva avverranno sui pixel "gia disegnati"
-        context.fillStyle = "#00d624"; //imposta il colore per le modifiche successive a verde 
-        context.fillRect(0, 0, canvas.width, canvas.height); //colora tutto il canva
-        context.globalCompositeOperation = "source-over"; //cambio le modifiche per applicarle sui pixel esterni
+        if(!kanaCorretti.includes(kanaImmagine) && !kanaSbagliati.includes(kanaImmagine)) //se kanaCorretti non contiene kanaImmagine e kanaSbagliati non contiene kanaImmagine
+        {
+            kanaCorretti.push(kanaImmagine);
+            context.globalCompositeOperation = "source-in"; //le modifice al canva avverranno sui pixel "gia disegnati"
+            context.fillStyle = "#00d624"; //imposta il colore per le modifiche successive a verde 
+            context.fillRect(0, 0, canvas.width, canvas.height); //colora tutto il canva
+            context.globalCompositeOperation = "source-over"; //cambio le modifiche per applicarle sui pixel esterni
         
-        immagine.src = canvas.toDataURL(); //carico il canva al posto dell'immagine
+            immagine.src = canvas.toDataURL(); //carico il canva al posto dell'immagine
 
-        setTimeout(function () { //dopo 1 secondo, verra ripristinata l'immagine con quella normale e successivamente invocato il metodo 'primoKana'
-            immagine.src = immagineNormale.src; //ripristino l'immagine con l'originale 
-            primoKana(); //chiamo il metodo
-        }, 1000);
+            setTimeout(function () { //dopo 1 secondo, verra ripristinata l'immagine con quella normale e successivamente invocato il metodo 'primoKana'
+                immagine.src = immagineNormale.src; //ripristino l'immagine con l'originale 
+                primoKana(); //chiamo il metodo
+            }, 1000);
 
-        document.getElementById("TextBox").value = ""; //svuoto il contenuto della textBox
+            document.getElementById("TextBox").value = ""; //svuoto il contenuto della textBox
+        }
+        else //altrimenti
+        {
+            context.globalCompositeOperation = "source-in"; //le modifice al canva avverranno sui pixel "gia disegnati"
+            context.fillStyle = "#00d624"; //imposta il colore per le modifiche successive a verde 
+            context.fillRect(0, 0, canvas.width, canvas.height); //colora tutto il canva
+            context.globalCompositeOperation = "source-over"; //cambio le modifiche per applicarle sui pixel esterni
+        
+            immagine.src = canvas.toDataURL(); //carico il canva al posto dell'immagine
+
+            setTimeout(function () { //dopo 1 secondo, verra ripristinata l'immagine con quella normale e successivamente invocato il metodo 'primoKana'
+                immagine.src = immagineNormale.src; //ripristino l'immagine con l'originale 
+                primoKana(); //chiamo il metodo
+            }, 1000);
+
+            document.getElementById("TextBox").value = ""; //svuoto il contenuto della textBox
+        }
     } 
-    else
+    else //altrimenti
     {
         console.log("Il testo non è corretto. Riprova."); //debug
 
-        context.globalCompositeOperation = "source-in"; //le modifice al canva avverranno sui pixel "gia disegnati"
-        context.fillStyle = "red"; //imposta il colore per le modifiche successive rosso 
-        context.fillRect(0, 0, canvas.width, canvas.height); //colora tutto il canva
-        context.globalCompositeOperation = "source-over"; //cambio le modifiche per applicarle sui pixel esterni
-        
-        immagine.src = canvas.toDataURL(); //carico il canva al posto dell'immagine
+        if(!kanaSbagliati.includes(kanaImmagine)) //se kanaSbagliati non contiene kanaImmagine
+        {
+            kanaSbagliati.push(kanaImmagine); //aggiungo kanaImmagine all'array di quelli sbagliati
 
-        setTimeout(function () { //ripristino l'immagine a quella normale dopo 1 secondo
-            immagine.src = immagineNormale.src;
-        }, 1000);
-        document.getElementById("TextBox").value = ""; //svuoto il contenuto della textBox
+            context.globalCompositeOperation = "source-in"; //le modifice al canva avverranno sui pixel "gia disegnati"
+            context.fillStyle = "red"; //imposta il colore per le modifiche successive rosso 
+            context.fillRect(0, 0, canvas.width, canvas.height); //colora tutto il canva
+            context.globalCompositeOperation = "source-over"; //cambio le modifiche per applicarle sui pixel esterni
+        
+            immagine.src = canvas.toDataURL(); //carico il canva al posto dell'immagine
+
+            setTimeout(function () { //ripristino l'immagine a quella normale dopo 1 secondo
+                immagine.src = immagineNormale.src;
+            }, 1000);
+            document.getElementById("TextBox").value = ""; //svuoto il contenuto della textBox
+        }
+        else //altrimenti
+        {
+            context.globalCompositeOperation = "source-in"; //le modifice al canva avverranno sui pixel "gia disegnati"
+            context.fillStyle = "red"; //imposta il colore per le modifiche successive rosso 
+            context.fillRect(0, 0, canvas.width, canvas.height); //colora tutto il canva
+            context.globalCompositeOperation = "source-over"; //cambio le modifiche per applicarle sui pixel esterni
+        
+            immagine.src = canvas.toDataURL(); //carico il canva al posto dell'immagine
+
+            setTimeout(function () { //ripristino l'immagine a quella normale dopo 1 secondo
+                immagine.src = immagineNormale.src;
+            }, 1000);
+            document.getElementById("TextBox").value = ""; //svuoto il contenuto della textBox            
+        }
+
+        
     }
 }
